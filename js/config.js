990a9f353258df043eae70ce446da009efa7f669
Sze Chung Lee <sclastro@gmail.com>
2026-05-22 19:09:28 +0800

    auto update

---
+++ js/config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const CONFIG = {
+  FH_KEY:  'd806uhhr01qj3ct99a30d806uhhr01qj3ct99a3g',
+  FH_BASE: 'https://finnhub.io/api/v1',
+  HOT_STOCKS: ['AAPL','TSLA','GOOGL','MSFT','NVDA','AMZN','META'],
+};
